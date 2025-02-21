bfe27f69ecfebaff20d909d8488d7848699ebde6
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-02-21 22:58:19 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,9 +25,10 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "succes" });
+app.post("/create-item", (req, res) => {});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
