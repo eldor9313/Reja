c7d7f13cace251f379bad4f076a120df4e8df4d7
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-02-27 11:43:40 +0900

    feat: Task B success

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-// Task
+// TASK ( A )
 
 //split("") + filter()
 
@@ -10,8 +10,28 @@
 
 //spread(...) + filter()
 
-function countLetter(harf, jumla) {
-  return [...jumla].filter((a) => a === harf).length;
+// function countLetter(harf, jumla) {
+//   return [...jumla].filter((a) => a === harf).length;
+// }
+
+// console.log(countLetter("e", "engineer"));
+
+// TASK  ( B )
+
+// function countDigits(mit) {
+//   let count = 0;
+//   for (let idx = 0; idx < mit.length; idx++) {
+//     if (mit[idx] >= "0" && mit[idx] <= "9") {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// console.log(countDigits("ad2a54y79wet0sfgb9"));
+
+function countDigits(str) {
+  return [...str].filter((char) => char >= "0" && char <= "9").length;
 }
 
-console.log(countLetter("e", "engineer"));
+console.log(countDigits("ad2a54y79wet0sfgb9"));
