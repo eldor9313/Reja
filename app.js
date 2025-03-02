bf2db19cf02ef253393b325ba8caeb4dbe2c7cf4
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-03-03 00:51:44 +0900

    feat: Reja mini loyihasi

--- app.js
+++ app.js
@@ -2,25 +2,16 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 
-// const fs = require("fs");
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
+
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
-
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -30,18 +21,19 @@ app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    res.json(data.ops[0]);
   });
 });
-
-// app.get("/author", (req, res) => {
-//   res.render("author", { user: user });
-// });
+// delete operations
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "succes" });
+    }
+  );
+});
 
 app.get("/", function (req, res) {
   console.log("user entered / ");
